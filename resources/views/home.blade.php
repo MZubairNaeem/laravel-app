3b55a6c675f4bf85ce5ed189c20dca20d6ef7371
M Zubair Naeem <zubairnaeem45@gmail.com>
2023-03-29 10:58:01 +0500

    adding todotask controller/model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
           
           <div class="hidden sm:block sm:ml-6">
             <div class="flex space-x-4">
-              <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+              <a  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
                </div>
           </div>
         </div>
@@ -29,69 +29,69 @@
     </div>
   </nav>
   
-    <div class="flex justify-center items-center h-screen  bg-cyan-600">
-        <div class="w-96 p-6 shadow-lg bg-white rounded-md">
-            <h1 class="text-3xl block text-center front-semibold">
-                TODO APP
-            </h1>
-            <hr class="mt-3">
-            <div class="mt-3">
-                
-                <input type="text" id="task" class="border w-full text-base px-2 py-1 focus:outline-none  focus:ring-0 focus:border-gray-600 " placeholder="Enter a task here" >
-            </div>
-            <div class="flex" >
-                <div class=" flex-1 mt-5">
-                    <button class="border-2  bg-cyan-500 text-white py-1 px-5 w-20 rounded-md">Save</button>
-                </div class="px-3">  
-                <div class="flex-1 mt-5">
-                    <button class="border-2  bg-cyan-500 text-white py-1 px-5 w-30 rounded-md">Get Tasks</button>
-                </div>
-
-            </div>
-           
-            <div>
-                <table class="table-auto w-full">
-                    <thead>
-                      <tr>
-                        <th class="px-4 py-2">S.NO.</th>
-                        <th class="px-4 py-2">Task</th>
-                        <th class="px-4 py-2">Status</th>
-                        <th class="px-4 py-2">Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td class="border px-4 py-2">1</td>
-                        <td class="border px-4 py-2">...</td>
-                        <td class="border px-4 py-2">...</td>
-                        <td class="border px-4 py-2">...</td>
-                      </tr>
-                      <tr>
-                        <td class="border px-4 py-2">2</td>
-                        <td class="border px-4 py-2">...</td>
-                        <td class="border px-4 py-2">...</td>
-                        <td class="border px-4 py-2">...</td>
-                      </tr>
-                      <tr>
-                        <td class="border px-4 py-2">3</td>
-                        <td class="border px-4 py-2">...</td>
-                        <td class="border px-4 py-2">...</td>
-                        <td class="border px-4 py-2">...</td>
-                      </tr>
-                      <tr>
-                        <td class="border px-4 py-2">4</td>
-                        <td class="border px-4 py-2">...</td>
-                        <td class="border px-4 py-2">...</td>
-                        <td class="border px-4 py-2">...</td>
-                      </tr>
-
-                    </tbody>
-                  </table>
-                  
+    <div class="flex justify-center items-center h-screen  bg-gray-300">
+      <div class="w-96 p-6 shadow-lg bg-white rounded-md">
+        <h1 class="text-3xl block text-center front-semibold">
+            TODO APP
+        </h1>
+        <hr class="mt-3">
+        <div class="mt-3">
             
-            </div> 
-           
+            <input type="text" id="task" class="border w-full text-base px-2 py-1 focus:outline-none  focus:ring-0 focus:border-gray-600 " placeholder="Enter a task here" >
         </div>
+        <div class="flex" >
+            <div class=" flex-1 mt-5">
+                <button class="border-2  bg-cyan-500 text-white py-1 px-5 w-20 rounded-md">Save</button>
+            </div class="px-3">  
+            <div class="flex-1 mt-5">
+                <button class="border-2  bg-cyan-500 text-white py-1 px-5 w-30 rounded-md">Get Tasks</button>
+            </div>
+
+        </div>
+       
+        <div>
+            <table class="table-auto w-full">
+                <thead>
+                  <tr>
+                    <th class="px-4 py-2">S.NO.</th>
+                    <th class="px-4 py-2">Task</th>
+                    <th class="px-4 py-2">Status</th>
+                    <th class="px-4 py-2">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="border px-4 py-2">1</td>
+                    <td class="border px-4 py-2">...</td>
+                    <td class="border px-4 py-2">...</td>
+                    <td class="border px-4 py-2">...</td>
+                  </tr>
+                  <tr>
+                    <td class="border px-4 py-2">2</td>
+                    <td class="border px-4 py-2">...</td>
+                    <td class="border px-4 py-2">...</td>
+                    <td class="border px-4 py-2">...</td>
+                  </tr>
+                  <tr>
+                    <td class="border px-4 py-2">3</td>
+                    <td class="border px-4 py-2">...</td>
+                    <td class="border px-4 py-2">...</td>
+                    <td class="border px-4 py-2">...</td>
+                  </tr>
+                  <tr>
+                    <td class="border px-4 py-2">4</td>
+                    <td class="border px-4 py-2">...</td>
+                    <td class="border px-4 py-2">...</td>
+                    <td class="border px-4 py-2">...</td>
+                  </tr>
+
+                </tbody>
+              </table>
+              
+        
+        </div> 
+       
+    </div>
         
     </div>
   
